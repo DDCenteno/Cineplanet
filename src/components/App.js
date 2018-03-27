5d478f501e3088b71625bd44ef0c3b370de573f6
JessicaMP <jessica95.mp@gmail.com>
2018-03-27 17:23:39 -0500

    Segunda vista

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer'
-
+import SignUp from './SignUp'
 import './App.css';
 
 const App = () => (
   <div className="container-fluid">
+    {/*Page 1*/}
     <Header />
     <Main />
     <Footer />
+    {/*Page 2*/}
+    <SignUp />
   </div>
 );
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,23 +3,23 @@ import React from 'react';
 const Footer = () => (
     <div className="col-12 mt-3">
         <div className="row justify-content-center align-items-center">
-        <div className="col-4 text-blue">
-        <div className="d-flex flex-column align-items-center">
-            <i className="fas fa-home fa-2x"></i>
-            <p>Inicio</p>
+            <div className="col-4 text-blue">
+                <div className="d-flex flex-column align-items-center">
+                    <i className="fas fa-home fa-2x"></i>
+                    <p>Inicio</p>
+                </div>
             </div>
+            <div className="col-4 text-blue">
+                <div className="d-flex flex-column align-items-center">
+                    <i className="fas fa-video fa-2x"></i>
+                    <p>Cines</p>
+                </div>
             </div>
-        <div className="col-4 text-blue">
-        <div className="d-flex flex-column align-items-center">    
-            <i className="fas fa-video fa-2x"></i>
-            <p>Cines</p>
-            </div>
-            </div>
-        <div className="col-4 text-blue">
-        <div className="d-flex flex-column align-items-center">    
-            <i className="fas fa-ellipsis-h fa-2x"></i>
-            <p>Más</p>
-            </div>
+            <div className="col-4 text-blue">
+                <div className="d-flex flex-column align-items-center">
+                    <i className="fas fa-ellipsis-h fa-2x"></i>
+                    <p>Más</p>
+                </div>
             </div>
         </div>
     </div>
