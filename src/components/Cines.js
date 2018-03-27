3e40c85b37e9d7f82d75f69d27cd9ea1d3562a12
AraceliGS <aragutarras@gmail.com>
2018-03-27 18:20:29 -0500

    ñadiendo ultima version del componente Cines

--- src/components/Cines.js
+++ src/components/Cines.js
@@ -23,6 +23,18 @@ const Cines = () => {
           <p>Tipo de salas</p>
         </div>
       </div>
+      <div className="col-12 mt-5">
+        <div className="d-flex justify-content-around align-items-center movies-list-container">
+        <div className="text-center d-flex align-items-center">
+          <span><i class="fas fa-heart"></i></span>
+          <div className="d-inline-block">
+            <h6 className="text-blue font-weight-bold">Cineplanet Salaverry</h6>
+            <span>Av. San Felipe Salaverry s/n</span>
+          </div>
+        </div>
+        <span className=""><i class="fas fa-chevron-right"></i></span>
+        </div>
+      </div>
     </div>
   )
 }
