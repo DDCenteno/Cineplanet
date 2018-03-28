7c75b483b23561a2412a0e82a8b971940897c49f
JessicaMP <jessica95.mp@gmail.com>
2018-03-27 21:09:56 -0500

    Agregando componentes reutilizables

--- src/components/Cines.js
+++ src/components/Cines.js
@@ -1,18 +1,11 @@
 import React from 'react';
+import HeaderCines from './HeaderCines';
+import Footer from './Footer';
 
 const Cines = () => {
   return (
     <div className="row mt-4">
-      <div className="col-12">
-        <div className="col-6 d-inline-block">
-          <span><i className="fas fa-chevron-left text-blue"></i></span>
-          <h4 className="ml-2 d-inline-block text-blue font-weight-bold">Cines</h4>
-        </div>
-        <div className="float-right">
-          <span><i className="fas fa-search fa-2x text-blue"></i></span>
-          <span className="ml-4"><i className="far fa-user-circle fa-2x text-blue"></i></span>
-        </div> 
-      </div>
+      <HeaderCines/>
       <div className="col-12 mt-4">
         <div className="col-6 d-inline-block text-center">
           <span><i className="fas fa-map-marker-alt"></i></span>
@@ -35,6 +28,7 @@ const Cines = () => {
         <span className=""><i className="fas fa-chevron-right"></i></span>
         </div>
       </div>
+      <Footer />
     </div>
   )
 }
---
+++ src/components/HeaderCines.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HeaderCines = () => (
+    <div className="col-12">
+        <div className="col-6 d-inline-block">
+          <span><i className="fas fa-chevron-left text-blue"></i></span>
+          <h4 className="ml-2 d-inline-block text-blue font-weight-bold">Cines</h4>
+        </div>
+        <div className="float-right">
+          <span><i className="fas fa-search fa-2x text-blue"></i></span>
+          <span className="ml-4"><i className="far fa-user-circle fa-2x text-blue"></i></span>
+        </div> 
+      </div>
+);
+
+export default HeaderCines;
