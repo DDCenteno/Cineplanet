e4b7765213126972137b3aeb35ac9aabf57f1138
JessicaMP <jessica95.mp@gmail.com>
2018-03-28 15:36:47 -0500

    Agregando vista Pago

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,49 @@
-import React, { Component }  from 'react';
-import Footer from './Footer';
-import MainNav from './MainNav'
-// import SignUp from './SignUp'
+// import React, { Component }  from 'react';
+import React from 'react';
+// import Main from './Main';
+// import Footer from './Footer';
+// import MainNav from './MainNav'
+// // import SignUp from './SignUp'
 // import Cines from './Cines';
 // import Confirm from './Confirm';
-// // import Seats from './Seats';
-// import Tickets from './Tickets';
+import Seats from './Seats';
+import Tickets from './Tickets';
+import Runway from './Runway';
 import './App.css';
 // Comento porque esta version de App es funcional
-// const App = () => (
-//   <div className="container-fluid">
-//     {/*Page 1*/}
-//     <Header />
-//     <Main />
-//     <Footer />
-//     {/*Page 2*/}
-//     <SignUp />
-//     {/*Page 3*/}
-//     <Cines />
-//      {/* Final Page */}
-//     <Confirm />
+const App = () => (
+  <div className="container-fluid">
+    {/*Page 1*/}
+    {/* <Main />
+    <Footer /> */}
+    {/*Page 2*/}
+    {/* <SignUp /> */}
+    {/*Page 3*/}
+    {/* <Cines /> */}
+     {/* Final Page */}
+    {/* <Confirm /> */}
 
-//     {/* Butacas */}
-//     {/* <Seats /> */}
+    {/* Butacas */}
+    <Seats />
     
-//     {/* Entradas */}
-//     <Tickets currentTicketPrice="14.00"/>
-//   </div>
-// );
+    {/* Entradas */}
+    <Tickets currentTicketPrice="14.00"/>
+    <Runway />
+  </div>
+);
 
-class App extends Component {
-  componentWillMount() {
-    this.props.getData()
-  }
+// class App extends Component {
+//   componentWillMount() {
+//     this.props.getData()
+//   }
 
-  render(){
-    return(
-    <div className="container-fluid">
-      <MainNav actualPage={ this.props.actualPage } navigateTo={ this.props.navigateTo } data={this.props.data}/>
-      <Footer navigateTo={this.props.navigateTo}/>
-    </div>)
-  }
-};
+//   render(){
+//     return(
+//     <div className="container-fluid">
+//       <MainNav actualPage={ this.props.actualPage } navigateTo={ this.props.navigateTo } data={this.props.data}/>
+//       <Footer navigateTo={this.props.navigateTo}/>
+//     </div>)
+//   }
+// };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { render } from 'react-dom';
+// import { render } from 'react-dom';
 import store from './store';
 import './index.css';
 import App from './containers/app';
